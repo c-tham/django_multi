fcf8bf927cbd33d2674c54eff99a99080ba885a7
Chia T <tham_oh@hotmail.com>
2018-07-24 00:47:38 -0500

    add features

--- mainApp/apps/IndexSite/urls.py
+++ mainApp/apps/IndexSite/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
  
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^feedback$', views.feedback),
+    url(r'^campus2$', views.campus2),
+    url(r'^campus3$', views.campus3),
 ]
--- mainApp/apps/IndexSite/views.py
+++ mainApp/apps/IndexSite/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 from django.shortcuts import render, HttpResponse, redirect
  
 def index(request):
-    context = {
-        "name" : "IndexSite"
-    }
-    return render(request, "IndexSite/index.html", context)
+    return render(request, "IndexSite/index.html")
+
+def feedback(request):
+    return render(request, "IndexSite/feedback.html")
+
+def campus2(request):
+    return render(request, "IndexSite/campus2.html")
+
+def campus3(request):
+    return render(request, "IndexSite/campus3.html")
