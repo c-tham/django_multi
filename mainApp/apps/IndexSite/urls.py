b8d79990a18e7c3f8c00cd43a47ba8bf0ba4679f
Chia T <tham_oh@hotmail.com>
2018-07-24 01:53:06 -0500

    bugs fixed

--- mainApp/apps/IndexSite/urls.py
+++ mainApp/apps/IndexSite/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^feedback$', views.feedback),
-    url(r'^campus2$', views.campus2),
-    url(r'^campus3$', views.campus3),
+    url(r'^campus2x$', views.campus2x),
+    url(r'^campus3x$', views.campus3x),
 ]
--- mainApp/apps/IndexSite/views.py
+++ mainApp/apps/IndexSite/views.py
@@ -9,8 +9,8 @@ def index(request):
 def feedback(request):
     return render(request, "IndexSite/feedback.html")
 
-def campus2(request):
-    return render(request, "IndexSite/campus2.html")
+def campus2x(request):
+    return render(request, "IndexSite/campus2x.html")
 
-def campus3(request):
-    return render(request, "IndexSite/campus3.html")
+def campus3x(request):
+    return render(request, "IndexSite/campus3x.html")
